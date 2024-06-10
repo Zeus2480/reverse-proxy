439fa5472e833687d3f0ffef1c4050cfe16ca284
Zeus2480 <siddiquifaizan2480@gmail.com>
2024-06-10 23:12:30 +0530

    Added db

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const httpProxy = require('http-proxy');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 const app = express();
 const PORT = 8000;
 const BASE_PATH = `https://vercel-clone-2480.s3.ap-south-1.amazonaws.com/__outputs/`
 
 const proxy = httpProxy.createProxy()
-app.use((req, res) => {
-    const hostName = req.hostname;
-    const subDomain = hostName.split('.')[0];
+app.use(async (req, res) => {
+    try {
+        const hostName = req.hostname;
+        const subDomain = hostName.split('.')[0];
+        const project = await prisma.project.findFirst({
+            where: { subDomain: subDomain }
+        });
+        const resolveTo = `${BASE_PATH}/${project.id}`;
+        return proxy.web(req, res, { target: resolveTo, changeOrigin: true });
 
-    const resolveTo = `${BASE_PATH}/${subDomain}`;
-    return proxy.web(req, res, { target: resolveTo, changeOrigin: true });
+    } catch (err) {
+        console.log(err);
+        return res.status(404).send('Not Found');
+    }
 })
 proxy.on('proxyReq', (proxyReq, req, res) => {
     const url = req.url;
